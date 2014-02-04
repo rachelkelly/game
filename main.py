d2e3c73e2d612e9635d55960d94bb4504b7f716d
rachelkelly <rachelkelly@users.noreply.github.com>
2014-02-03 23:54:42 -0800

    added an inventory class but not sure how to go!

--- main.py
+++ main.py
@@ -10,12 +10,41 @@ class Room(object):
 		# these are placeholdery, I feel like putting "pass"
 		# in the __init__ fn would be a bad time at this juncstuah
 		# zed did also say to not put too much in the __init__
+		# ... so what DO I put into it
 		pass
+
+class Inventory(Room):
+	#why am I calling Room?  hmmm bc you have to be IN the room
+	#to instantiate the Inventory class, maybe?
+	#ok but all in all this class is looking better, if
+	#only I could just GET to it, esp now that I've got the
+	#invlist as part of the megafauna!
+
+	def inventorycheck(self):
+		print "inventory:\n %s" % invlist
+
+	def inventoryadd(self, addnew):
+		#don't know if putting var addnew in here is necessary right now
+		if addnew in invlist:
+			print "can't have more than one, homes"
+		elif addnew not in invlist:
+			print "cool!  now the %s is in your inventory." %addnew
+			invlist.append(addnew)
+			print "want to see what's in the bag now?"
+			invchk = raw_input("> ")
+			if 'y' in invchk:
+				self.inventorycheck()
+			elif 'n' in invchk:
+				print "ok you don't have to, anyway it's over now"
+				exit(0)
+		else:
+			print "error in adding item to inventory"	
 
 
 class BookRoomHaHa(Room):
 	def book(self):
 		print "ok, you ate the book."
+		exit(0)
 
 	def bookviewing(self):
 		print "you're in a room. who knows what kind yet?"
@@ -31,12 +60,24 @@ class BookRoomHaHa(Room):
 			print "you see a few books I guess, I don't know, man!"
 		elif "thing2" in get_it_done:
 			print "thing 2"
+		elif "get" in get_it_done:
+			print "buddy, you got it."
+			print "for now, just tell me what you got, again: "
+			addnew = raw_input("> ")
+			#print "oktest!"
+			# ok this piece is fine, now how to jump from one class
+			# to another
+			# why not just:
+			#Inventory.inventoryadd()
+			# doesn't work woh wohhhh
 		else:
 			print "try again doof"
+			self.bookviewing()
 
 
 class DontDoIt(Room):
 	pass
 
+invlist = []
 crum = BookRoomHaHa()
 crum.bookviewing()
